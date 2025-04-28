6fc6cb8ef57b91ed8ff1ab7979355070f4637fe1
Ajmal Sadath <56781315+ajmal123-coder@users.noreply.github.com>
2025-04-28 07:29:45 +0530

    Time: 15 ms (21.96%), Space: 45.2 MB (19.77%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,10 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
        String cs = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
-       StringBuilder ns =new StringBuilder();
-       for(int i = cs.length()-1;i>=0;i--){
-            ns.append(cs.charAt(i));
-       }
-       return cs.equals(ns.toString());
+       return cs.equals(new StringBuilder(cs).reverse().toString());
+       
     }
 }
